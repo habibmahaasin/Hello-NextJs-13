ea1c12652055a8beaedc5e391f8a94f77e0cdce8
Habibmahaasin <habibirf23@gmail.com>
2023-01-20 23:30:04 +0700

    Revert "test"
    This reverts commit a290926ef58ae2e130ff217e8a36d05abc386eb4.

--- project-001/pages/projects/index.js
+++ project-001/pages/projects/index.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import Image from "next/legacy/image";
+import Image from 'next/image'
 
 function ProjectsPage() {
   const [isLoading,setIsLoading] = useState(true)
